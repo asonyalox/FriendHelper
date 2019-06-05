157d58fb9ffca864316f8f60f37dbd103e01d894
Misha Lytva <asonyalox@gmail.com>
2019-06-05 14:06:29 +0300

    Add project files.

---
+++ Friender/Models/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Friender.Models
+{
+    public class Event
+    {
+       public int Id { get; set; }
+       public string Photos { get; set; }
+       public string Category { get; set; }
+       public DateTime Dates { get; set; }
+       public string Owner { get; set; }
+       public string Description { get; set; }         
+
+    }
+}
---
+++ Friender/Models/FrienderContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Friender.Models
+{
+    public class FrienderContext: IdentityDbContext<User>
+    {
+        public FrienderContext(DbContextOptions<FrienderContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+    }
+}
